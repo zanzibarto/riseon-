f6fcb2042087454bab7780f2b53af2c7dc5d7f64
zanzibarto <zanzibarto2017@gmail.com>
2025-04-03 14:20:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -37,10 +37,13 @@ app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect(err => {
